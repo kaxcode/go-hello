37281d81cc02fc6a263776cb851d5b98d3ade75b
Kenia <keniavalladarez@gmail.com>
2018-08-27 14:50:36 -0600

    Creating and using a local package

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func main() {
+	b, err := ioutil.ReadFile("proverbs.txt")
+	if err != nil {
+		os.Exit(1)
+	}
+
+	lines := strings.SplitAfter(string(b), ".")
+
+	for _, str := range lines {
+		m := map[string]int{}
+		for _, r := range str {
+			c := string(r)
+			m[c]++
+		}
+		fmt.Println(str, m)
+	}
+}
